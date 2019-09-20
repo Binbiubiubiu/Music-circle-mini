87faef10ebdb5a3153d5544d3da6af0ba4a502f7
Wildlifes <2632003786@qq.com>
2019-09-21 00:30:34 +0800

    Initial Commit

---
+++ components/search-bar/index.js
@@ -0,0 +1,26 @@
+// components/search-bar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    disabled: {
+      type: Boolean,
+      value: false
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,78 @@
+// index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selectedTabIndex:0,
+    tabs:["推荐","MV","电台","排行"]
+  },
+  handleTabTap(event){
+    const target = event.target;
+    this.setData({
+      selectedTabIndex: target.dataset.index
+    })
+  },
+  goToSearchPage(){
+    wx.navigateTo({
+      url: '/pages/search/index',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
